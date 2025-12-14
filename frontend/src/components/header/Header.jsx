322141298a2c83e667d68d6a978b4e05358f8285
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-14 15:30:08 +0300

    add icons and items to Header and add Styles

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -8,6 +8,7 @@ export const Header = () => {
                     <i class="bi bi-list"></i>
                 </div>
                 <div className="header-top-phone">
+                    <i className="bi bi-telephone-fill"></i>
                     0552666666
                 </div>
                 <div className="header-top-text">
@@ -16,6 +17,20 @@ export const Header = () => {
                 <div className="header-top-link">
                     <i className="bi bi-person-fill"></i>
                     Login
+                </div>
+            </div>
+            <div className="header-middle">
+                <div className="header-middle-logo">
+                    <b>Book</b>
+                    <i className='bi bi-book'></i>
+                    <b>Store</b>
+                </div>
+                <div className="header-middle-search-box">
+                    <input placeholder='Search in our Book Store ....' type="text" className="header-middle-search-input" />
+                    <i className="bi bi-search"></i>
+                </div>
+                <div className="header-middle-cart-wrapper">
+                    <i className="bi bi-cart2"></i>
                 </div>
             </div>
         </header>
