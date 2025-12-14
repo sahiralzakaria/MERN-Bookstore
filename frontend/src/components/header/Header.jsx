4eeeeefc01c7c39cb2579c8779eb605d5628fa19
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-14 17:29:01 +0300

    add navbar items and styles

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,12 +1,14 @@
 import './header.css'
 import HeaderMiddle from './HeaderMiddle'
 import HeaderTop from './HeaderTop'
+import Navbar from './Navbar'
 
 export const Header = () => {
     return (
         <header className="header">
             <HeaderTop />
             <HeaderMiddle />
+            <Navbar />
         </header>
     )
 }
---
+++ frontend/src/components/header/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul className="navbar-links">
+                <li className="navbar-link">Home</li>
+                <li className="navbar-link">Authors</li>
+                <li className="navbar-link">About Us</li>
+                <li className="navbar-link">Contact Us</li>
+                <li className="navbar-link">Register</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
