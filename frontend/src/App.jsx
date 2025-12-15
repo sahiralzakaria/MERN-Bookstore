4b8ad553da4e9621c41b9adf03b144a79835b279
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-15 15:05:43 +0300

    create Heading Title

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import BookSlider from './components/book-slider/BookSlider'
 import { Header } from './components/header/Header'
+import HeadingTitle from './components/heading-title/HeadingTitle'
 import Services from './components/services/Services'
 import Slider from './components/slider/Slider'
 import { books } from './data/books'
@@ -13,6 +14,11 @@ function App() {
       <Header />
       <Slider />
       <Services />
+      <HeadingTitle title='Best Seller' />
+      <BookSlider data={books} />
+      <HeadingTitle title='Most gifted' />
+      <BookSlider data={books} />
+      <HeadingTitle title='Most wished for' />
       <BookSlider data={books} />
     </div>
   )
