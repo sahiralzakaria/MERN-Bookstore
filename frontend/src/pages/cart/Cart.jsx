749132a88b23abe625d2e2beba3ad996f072aedd
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-16 22:43:05 +0300

    add items to cart page

--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import { cartInfo } from '../../data/cart'
 
 const Cart = () => {
     return (
-        <h1>Cart</h1>
+        <div className="cart">
+            <h1 className="cart-title">Your Shopping Cart</h1>
+            <div className="cart-wrapper">
+                <div className="cart-items">
+                    {cartInfo.map((item) => (
+                        <div key={item.id} className="cart-item">
+                            <img
+                                src={`/books/${item.image}`}
+                                alt={item.title}
+                                className="cart-item-img"
+                            />
+                            <div className="cart-item-info">
+                                <div>
+                                    <div className="cart-item-book-title">
+                                        <b>Title: </b>
+                                        {item.title}
+                                    </div>
+                                    <div className="cart-item-author">
+                                        <b>Author: </b>
+                                        {item.author}
+                                    </div>
+                                </div>
+                                <div>
+                                    <div className="cart-item-quantity">
+                                        <button
+                                            className="bi bi-plus-lg"
+                                        ></button>
+                                        <b>{item.quantity}</b>
+                                        <button
+                                            disabled={item.quantity <= 1}
+                                            className="bi bi-dash-lg"
+                                        ></button>
+                                    </div>
+                                    <div className="cart-item-price">
+                                        ${(item.price * item.quantity).toFixed(2)}
+                                    </div>
+                                    <i
+                                        className="bi bi-trash-fill"
+                                    ></i>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+
+            </div>
+        </div>
     )
 }
 
