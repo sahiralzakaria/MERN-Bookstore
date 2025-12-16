7d005255e170570d6032217eb5cdb16c42256835
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-16 22:51:01 +0300

    add cart page styles

--- frontend/src/data/cart.js
+++ frontend/src/data/cart.js
@@ -1,6 +1,6 @@
 export const cartInfo = [
     {
-        id:1,
+        id: 1,
         bookId: 8,
         title: "Never Split The Difference",
         image: "never-split-the-difference.jpg",
@@ -9,12 +9,31 @@ export const cartInfo = [
         author: "Chris Voss",
     },
     {
-        id:2,
+        id: 2,
         bookId: 5,
         title: "Rich Dad Poor Dad",
         image: "rich-dad-poor-dad.jpg",
         price: 8,
         quantity: 3,
         author: "Robert T.Kiyosaki",
+    },
+    {
+
+        id: 3,
+        bookId: 9,
+        title: "The Alchemist",
+        image: "the-alchemist.jpg",
+        price: 9.25,
+        quantity: 1,
+        author: "Paulo Coelho",
+    },
+    {
+        id: 4,
+        bookId: 11,
+        title: "Harry Potter and the Sorcerer's Stone",
+        image: "harry-potter.jpg",
+        price: 10,
+        quantity: 2,
+        author: "J.K. Rowling",
     },
 ]
--- frontend/src/pages/cart/Cart.jsx
+++ frontend/src/pages/cart/Cart.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import './cart.css'
+
 import { cartInfo } from '../../data/cart'
 
 const Cart = () => {
