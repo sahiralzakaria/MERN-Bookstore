54bdd158680c3ca68b8feaaa641027020ec502fa
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-20 15:59:59 +0300

    add items to about page and styles

--- frontend/src/pages/about/About.jsx
+++ frontend/src/pages/about/About.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
-
+import './about.css'
 const About = () => {
     return (
-        <h1>About</h1>
+        <section className="about">
+            <h1>About Me</h1>
+
+            <div className="about-container">
+
+                {/* Image Placeholder */}
+                <div className="about-image">
+                    <img src="/profImg.png" alt="Sahir ALZAKARIA" />
+                </div>
+
+                {/* Content */}
+                <div className="about-content">
+                    <p>
+                        <strong>Bachelor’s Degree in Computer Engineering</strong>
+                    </p>
+
+                    <p>
+                        2021 – 2025 <br />
+                        Inonu University (İnönü Üniversitesi)
+                    </p>
+
+                    <p>
+                        Syrian | Based in Turkey
+                    </p>
+
+                    <div className="about-links">
+                        <p>
+                            📧 <a href="mailto:sahir.alzakaria@gmail.com">
+                                sahir.alzakaria@gmail.com
+                            </a>
+                        </p>
+
+                        <p>
+                            🔗 <a
+                                href="https://linkedin.com/in/sahir-zakaria"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                linkedin.com/in/sahir-zakaria
+                            </a>
+                        </p>
+
+                        <p>
+                            💻 <a
+                                href="https://github.com/sahiralzakaria"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                github.com/sahiralzakaria
+                            </a>
+                        </p>
+                    </div>
+                </div>
+
+
+            </div>
+        </section>
     )
 }
 
