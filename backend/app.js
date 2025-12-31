3df324e86cc9ebcf291c77d31bd23bee5a99b140
sahir zakaria <sahir.alzakaria@gmail.com>
2025-12-31 17:38:42 +0300

    add update user with authorization by token

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const booksPath = require('./routes/books')
 const authorsPath = require('./routes/authors')
 const authPath = require('./routes/auth')
+const usersPath = require('./routes/users')
 const mongoose = require('mongoose');
 const logger = require('./middlewares/logger');
 const { notFound, errorHanlder } = require('./middlewares/errors')
@@ -25,6 +26,7 @@ app.use(logger);
 app.use('/api/books', booksPath);
 app.use('/api/authors', authorsPath);
 app.use('/api/auth', authPath);
+app.use('/api/users', usersPath);
 
 //Error Handler Middleware
 app.use(notFound)
