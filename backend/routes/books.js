248650066d33ef90232db6f343c7a66b48a45e1e
sahir zakaria <sahir.alzakaria@gmail.com>
2026-01-03 15:25:32 +0300

    refactoring

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const { verifyTokenAndAdmin } = require("../middlewares/verifyToken");
-const { getAllBooks, getBookById, createBook, bookUpdate, deleteBook } = require('../controllers/bookController');
+const {
+    getAllBooks,
+    getBookById,
+    createBook,
+    bookUpdate,
+    deleteBook
+} = require('../controllers/bookController');
 
+// /api/books
+router.route("/")
+    .get(getAllBooks)
+    .post(verifyTokenAndAdmin, createBook);
 
-router.get('/', getAllBooks);
-
-router.get('/:id', getBookById);
-
-router.post('/', verifyTokenAndAdmin, createBook);
-
-router.put('/:id', verifyTokenAndAdmin, bookUpdate);
-
-router.delete('/:id', verifyTokenAndAdmin, deleteBook)
-
-
+// /api/books/:id
+router.route("/:id")
+    .get(getBookById)
+    .put(verifyTokenAndAdmin, bookUpdate)
+    .delete(verifyTokenAndAdmin, deleteBook);
 
 module.exports = router;
 
